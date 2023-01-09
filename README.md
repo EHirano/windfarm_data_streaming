1faee6dd2169b8a85369b99f9294ca394e77e82f
Eduardo Lovera Hirano <eduardoloverahirano@gmail.com>
2023-01-09 00:14:43 -0300

    feat: created data generators for hydraulic pressure, temperature and powerfactor ingested by Kinesis

--- README.md
+++
@@ -1,2 +0,0 @@
-# windfarm_data_streaming
-This project simulates IoT data from sensors installed in a wind farm. The data is ingested into AWS using Kinesis Data Stream and Kinesis Data Firehose and stored in an S3 bucket. Then, a Glue Crawler creates a Glue Catalog that is used by a Glue Job to transfer the data to another S3 bucket, which can be then queried using Athena.
---
+++ temperature.py
@@ -0,0 +1,31 @@
+import boto3
+import json
+from random import uniform
+import time
+from datetime import datetime
+
+# O ideal, segundo a documentação, seria provisionar uma IAM Role para aplicações efêmeras.
+client = boto3.client('kinesis',
+    aws_access_key_id = 'AKIAY24ZCR64F2SAOAF3',
+    aws_secret_access_key = '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',
+    region = 'sa-east-1'
+    )
+
+id = 0
+while True:
+    data = uniform(20, 25)
+    id += 1
+    input = {
+        'idtemp': str(id),
+        'data': str(data),
+        'type': 'temperature',
+        'timestamp': str(datetime.now())
+        } 
+
+    client.put_record(
+        StreamARN='windfarm',
+        Data = json.dumps(input),
+        PartitionKey = '02'
+    )
+    
+    time.sleep(10)
